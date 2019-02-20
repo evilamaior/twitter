d3963db68e7218c619813a20a177f26383533b32
evilamaior <contatoevilamaior@gmail.com>
2019-02-19 22:30:27 -0300

    VERSION 0.0.3

--- app.js
+++ app.js
@@ -22,11 +22,18 @@ tweetText.addEventListener("keyup", startCounter)
 function startCounter(event) {
     let counterValue = maxTweetLength - tweetText.value.length;
     counter.textContent = counterValue;
-    if (counterValue === maxTweetLength) {
+    
+    if (counterValue === maxTweetLength || counterValue < 0) {
         tweetButton.setAttribute("disabled", "");
     }else {
         tweetButton.removeAttribute("disabled");
     }
 
-
+    if (tweetText.value.length > 130) {
+        counter.setAttribute("class","red");
+    }else if (tweetText.value.length > 120) {
+        counter.setAttribute("class","yellow");
+    }else {
+        counter.setAttribute("class","blue");
+    }
 }
