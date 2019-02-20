b20ac47cdcf64e8997c94095d5a680f1139a7131
evilamaior <contatoevilamaior@gmail.com>
2019-02-19 22:02:23 -0300

    VERSION 0.0.2

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ let tweetButton = document.querySelector("#tweetButton");
 
 let tweetList = document.querySelector("#tweetList");
 
+let counter = document.querySelector("#counter");
+
+const maxTweetLength = 140;
+
 tweetButton.addEventListener("click", addTweet);
 
 function addTweet(event) {
@@ -11,4 +15,18 @@ function addTweet(event) {
     listItem.textContent = tweetText.value;
     tweetList.appendChild(listItem);
     tweetText.value = "";
+}
+
+tweetText.addEventListener("keyup", startCounter)
+
+function startCounter(event) {
+    let counterValue = maxTweetLength - tweetText.value.length;
+    counter.textContent = counterValue;
+    if (counterValue === maxTweetLength) {
+        tweetButton.setAttribute("disabled", "");
+    }else {
+        tweetButton.removeAttribute("disabled");
+    }
+
+
 }
